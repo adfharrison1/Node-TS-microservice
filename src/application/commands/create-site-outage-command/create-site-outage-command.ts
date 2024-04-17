c8e67956c8634669dfa5f87fc158217e158fc1d7
AlanHarrisonPeak <alan.harrison@peak.ai>
2024-04-17 19:00:04 +0100

    refactor: amend readme

--- src/application/commands/create-site-outage-command/create-site-outage-command.ts
+++ src/application/commands/create-site-outage-command/create-site-outage-command.ts
@@ -1,11 +1,28 @@
+import { EnhancedOutage } from "@domain/entities";
 import { Dependencies } from "@infrastructure/di";
 
 export type CreateSiteOutageCommand = {
     siteId: string;
+    outages: Array<{
+        id: string,
+        name: string,
+        begin: string
+        end: string
+    }>
 }
 
-export function makeCreateSiteOutageCommand({} : Dependencies){
+export function makeCreateSiteOutageCommand({krakenTestService} : Dependencies){
     return async function createSiteOutageCommand(command: CreateSiteOutageCommand) {
-        return `siteId for outages was: ${command.siteId}`;
+
+        const outages = command.outages.map((outage) => {
+            return new EnhancedOutage({
+                id: outage.id,
+                name: outage.name,
+                begin: new Date(outage.begin),
+                end: new Date(outage.end)
+            })
+        })
+        
+        return krakenTestService.createOutages({siteId: command.siteId, outages});
     }
 }
--- src/web/controllers/site-outages-controller.ts
+++ src/web/controllers/site-outages-controller.ts
@@ -3,10 +3,11 @@ import { IRouter } from 'express';
 export function siteOutagesController({ router }: { router: IRouter }) {
     router.post('/site-outages/:siteId', async function createSiteOutageBySiteId(request, response, next) {
         try {
-
+            console.log(request.body)
         // resolve the command needed off the container, which Awilix will handle all the dependency injection, scoping and instantiation for
         const result = await request.container.resolve('commands').createSiteOutageCommand({
             siteId: request.params.siteId,
+            outages: request.body
         });
 
         return response.status(200).json(result);
