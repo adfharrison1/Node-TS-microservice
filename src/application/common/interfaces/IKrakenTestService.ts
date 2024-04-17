cb4bdadec0407a4d86de01f2cf93783c35dc1655
AlanHarrisonPeak <alan.harrison@peak.ai>
2024-04-17 16:42:58 +0100

    feat: implement interface for kraken service

--- src/application/common/interfaces/IKrakenTestService.ts
+++ src/application/common/interfaces/IKrakenTestService.ts
@@ -1,5 +1,4 @@
 import { EnhancedOutage, Outage, Site } from "@domain/entities";
-import { VariableDeclarationList } from "typescript";
 
 // implement an interface over the infrastructure concern that calls the test API - this means that as long as the underlying
 // concern always conforms to this interface, the application layer doesnt need refactoring when the concern changes 
